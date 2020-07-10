12cc251f98428632be42b485b938d2e0f966f153
RocketRobz <bobesh8@gmail.com>
2020-07-10 02:13:57 -0600

    Bug fix

--- source/screens/rocketRobz.cpp
+++ source/screens/rocketRobz.cpp
@@ -23,7 +23,7 @@ void RocketRobz::Draw(void) const {
 		if (cinemaWide) {
 			GFX::DrawSprite(sprites_logo_rocketrobz_idx, 60, 36, 0.35f, 0.7f, GPU_LINEAR);
 		} else {
-			GFX::DrawSprite(sprites_logo_rocketrobz_idx, 0, 0, 0.5, 1, (gfxIsWide() ? GPU_LINEAR : GPU_NEAREST));
+			GFX::DrawSprite(sprites_logo_rocketrobz_idx, 0, 0, 0.5, 1, (gfxIsWide() ? GPU_NEAREST : GPU_LINEAR));
 		}
 		Gui::Draw_Rect(0, 238, 400, 2, C2D_Color32(0, 0, 0, 255));	// Hide line from other texture(s)
 		Gui::DrawString(8, (cinemaWide ? 182 : 218)-(shiftBySubPixel ? 0.5f : 0), 0.50, WHITE, this->yearText);
